027b3dec47a76d507164b5574833c66c0cdd49d5
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-07-01 01:12:14 +0530

    Update language.config.js

--- configs/language.config.js
+++ configs/language.config.js
@@ -1,4 +1,4 @@
-const { CPP, C, PYTHON, JAVA, NODEJS, RUBY, PROMPTV1, PROMPTV2 } = require('../enums/supportedLanguages')
+const { CPP, C, PYTHON, JAVA, NODEJS, RUBY,GO,RUST,SWITFT, PROMPTV1, PROMPTV2 } = require('../enums/supportedLanguages')
 const ONE_MB = 1024 // ulimit uses Kilobyte as base unit
 const ALLOWED_RAM = process.env.ALLOWED_RAM || 512
 
@@ -43,6 +43,27 @@ const LANGUAGES_CONFIG = {
         run: 'ruby solution.rb',
         timeout: 10,
         filename: 'solution.rb',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [GO]: {
+        compile: 'go build -o solution solution.go',
+        run: './solution',
+        timeout: 2,
+        filename: 'solution.go',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [RUST]: {
+        compile: 'rustc -o solution solution.rs',
+        run: './solution',
+        timeout: 3,
+        filename: 'solution.rs',
+        memory: ALLOWED_RAM * ONE_MB,
+    },
+    [SWIFT]: {
+        compile: 'swiftc -o solution solution.swift',
+        run: './solution',
+        timeout: 3,
+        filename: 'solution.swift',
         memory: ALLOWED_RAM * ONE_MB,
     },
     [PROMPTV1]: {
